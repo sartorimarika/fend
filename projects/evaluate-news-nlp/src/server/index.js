e73e144b738c4ee4192ecefdfd67515d7737c69d
Marika Sartori <marikasartori@MacBook-Pro-di-Marika.local>
2022-03-20 22:59:46 +0100

    server + webpack

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,12 +1,24 @@
 var path = require('path')
-const express = require('express')
+const express = require('express') // express to run server and routes
 const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
+const app = express() //start up an instance of app
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
+
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -14,6 +26,7 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
+// Create a local server
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
