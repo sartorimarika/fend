1209cd51a7bfeadfe44ee18e2cea105a26ba0d3b
Marika Sartori <marikasartori@MacBook-Pro-di-Marika.local>
2022-03-23 22:38:13 +0100

    sass loader working

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -22,7 +22,7 @@ app.use(cors())
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
